981904b29f665b0af313a279494cca9d6fee83ac
Bo Li <drluodian@gmail.com>
2021-08-09 01:41:33 +0800

    update dataset name

--- amlt_formatter.py
+++ amlt_formatter.py
@@ -13,4 +13,20 @@ def IIB():
             print(formatted_string.format(algo=algo, dataset=dataset))
 
 
-IIB()
+def pruner():
+    print('jobs:')
+    formatted_string = "  - name: cifar100_prune_{model}_{algo}\n" \
+                       "    sku: G1\n" \
+                       "    sku_count: 1\n" \
+                       "    aml_mpirun:\n" \
+                       "      process_count_per_node: 1\n" \
+                       "      communicator: \"OpenMpi\"\n" \
+                       "    command:\n" \
+                       "      - python basic_pruners.py --dataset cifar100 --model {model} --pruner {algo} --pretrain-epochs 160 --fine-tune-epochs 160 --speed-up --pretrained-model-dir ./experiment_data/pretrain_cifar100_{model}.pth --enable-trace 2>&1 | tee $$AMLT_OUTPUT_DIR/pruning_cifar100_{model}_{algo}.txt"
+
+    for algo in ['l1filter', 'l2filter', 'fpgm', 'apoz', 'mean_activation', 'taylorfo']:
+        for model in ["resnet18", "resnet34", "resnet50", "vgg16", "vgg19", "mobilenet_v1", "mobilenet_v2"]:
+            print(formatted_string.format(algo=algo, model=model))
+
+
+pruner()
